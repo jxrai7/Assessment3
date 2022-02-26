84e8c88300deb2ef8229c2cd87ee42822ba0deae
Jasxrai7 <raikjas@gmail.com>
2022-02-26 16:46:34 -0500

    Update settings.py
    removed secret key

--- wackywidgetsjas/settings.py
+++ wackywidgetsjas/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%#12z-qola0hz@^=x4d3j6n6qf-vi@t)=jd63+wx$mnj!w!+px'
+SECRET_KEY = 'django-insecure-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
